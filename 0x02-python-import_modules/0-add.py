e56b46395f2a6df516a3ac7a548ab2eae94be511
ouailb <adomeyou@gmail.com>
2023-09-11 12:47:58 +0100

    t0 +

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
+from add_0 import add
 # Assign the values to variables a and b
 a = 1
-b = 7
+b = 2
 
 # Import the add function from add_0 module
-from add_0 import add
+
 
 # Calculate the result using the add function
 result = add(a, b)
