e0d5cad6b84961a63a0bf60764b3817fabe3add1
ouailb <adomeyou@gmail.com>
2023-09-11 12:50:05 +0100

    t0 ++

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-# Assign the values to variables a and b
 a = 1
 b = 2
-
-# Import the add function from add_0 module
-
-
-# Calculate the result using the add function
 result = add(a, b)
-
-# Print the result using string formatting
 print("{} + {} = {}".format(a, b, result))
