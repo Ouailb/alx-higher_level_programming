b3f4bf4074a2473028d896ec3618040228da668a
ouailb <adomeyou@gmail.com>
2023-09-26 13:42:29 +0100

    t0+++++++

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,16 +9,15 @@
         pass
     finally:
         print()
-    return count
-'''
+    return count '''
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-        print()  # Add a newline after printing the elements
+        print()
         return count
     except IndexError:
-        print()  # Add a newline if an IndexError occurs
+        print()
         return count
