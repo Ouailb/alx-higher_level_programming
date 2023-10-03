c21932b1b13b83e67038011eb8dcc4083f8d9249
ouailb <adomeyou@gmail.com>
2023-10-03 10:26:32 +0100

    t1++++

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,6 +13,11 @@ class Rectangle:
         """width"""
         return self.__width
 
+    @property
+    def height(self):
+        """hight"""
+        return self.__height
+
     @width.setter
     def width(self, value):
         """width error"""
@@ -22,11 +27,6 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        """hight"""
-        return self.__height
 
     @height.setter
     def height(self, value):
