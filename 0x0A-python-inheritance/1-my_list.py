26e830881cdf2bcdc6f2c7b7b9786834aab15085
ouailb <adomeyou@gmail.com>
2023-10-09 16:47:57 +0100

    t1++

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
 """ Print the list in ascending sorted order """
 
 
 class MyList(list):
+    """
+        Print the list in ascending sorted order.
+     """
+
     def print_sorted(self):
         """
         Print the list in ascending sorted order.
