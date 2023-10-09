d867c8fe36fd733acafe76e24aea932c4645ebdb
ouailb <adomeyou@gmail.com>
2023-10-09 21:06:14 +0100

    t5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    An empty base class for geometry-related operations.
+    """
+
+
+class BaseGeometry:
+    """
+    An empty base class for geometry-related operations.
+    """
+    pass
