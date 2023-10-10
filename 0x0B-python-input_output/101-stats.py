1d306d50446270530634701dae6371b8aa45899f
ouailb <adomeyou@gmail.com>
2023-10-10 22:16:44 +0100

    t14++

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,53 +1,59 @@
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
 
-""" Reads from standard input and computes metrics """
-
-
-import signal
-import sys
-
-
-def print_stats(signum, frame):
-    """Print accumulated metrics."""
-    total_size = sum(file_sizes)
-    print("File size: {}".format(total_size))
-
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
-signal.signal(signal.SIGINT, print_stats)
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-status_codes = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-file_sizes = []
 
-line_count = 0
+if __name__ == "__main__":
+    import sys
 
-for line in sys.stdin:
-    parts = line.split()
-    if len(parts) > 2:
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-        if status_code in status_codes:
-            status_codes[status_code] += 1
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-        file_sizes.append(file_size)
-        line_count += 1
+            line = line.split()
 
-        if line_count == 10:
-            print_stats(0, 0)
-            line_count = 0
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-print_stats(0, 0)
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
