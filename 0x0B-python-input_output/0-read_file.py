459a586c4bbf64e1374aba7a553cb2fabade9f4d
ouailb <adomeyou@gmail.com>
2023-10-10 16:29:10 +0100

    t0+

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,3 +8,10 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line, end="")
+
+    # with open(filename, 'r', encoding="utf-8") as f:
+    # read_data = f.read()
+
+    #  print(read_data, end='')
+
+    # print(f.read(), end="")
