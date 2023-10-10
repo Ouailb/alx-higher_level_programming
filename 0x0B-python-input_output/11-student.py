c62e963874e00a208cc84bfec0972d6c87ae79e4
ouailb <adomeyou@gmail.com>
2023-10-10 21:24:38 +0100

    t11+

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
 
+""" Module that defines the class Student """
 
-Student = __import__('10-student').Student
 
-def reload_from_json(self, json):
+class Student:
+    """ Class to create student instances """
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that returns """
+        if attrs is None:
+            return self.__dict__.copy()
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+
+    def reload_from_json(self, json):
         """replaces all attributes of the Student"""
         for key, value in json.items():
             setattr(self, key, value)
