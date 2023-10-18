33a965917c48ddf8034142cc7f4c078d0e1f87d5
ouailb <adomeyou@gmail.com>
2023-10-18 16:00:05 +0100

    t8++

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,14 +100,16 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args, **kwargs):
-        """ Update attributes with a combination of
-        positional and keyword arguments """
+    def update(self, *args):
+        """ Update attributes with no-keyword arguments """
         if args:
-            attrs = ["id", "width", "height", "x", "y"]
-            for i, value in enumerate(args):
-                if i < len(attrs):
-                    setattr(self, attrs[i], value)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
