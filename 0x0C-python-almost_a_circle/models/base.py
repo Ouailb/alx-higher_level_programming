ec45435a2fc505e01d7139907b73659a1da364b2
ouailb <adomeyou@gmail.com>
2023-10-19 12:10:41 +0100

    t15+++

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-    class Base
-"""
+
+""" Base class """
+
 
 import json
 
 
 class Base:
-
-    """ mother :)
-    This class will manage the id attribute for all the classes
-    Args:
-    @id: The id for a specific instance.
-    """
-
+    """ class base """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ init """
         if id is not None:
             self.id = id
         else:
@@ -25,8 +20,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts"""
-
-        if list_dictionaries is None or list_dictionaries == []:
+        """ JSON. """
+        if list_dictionaries is None or len(list_dictionaries) == []:
             return "[]"
         return json.dumps(list_dictionaries)
