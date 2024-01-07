9f89e923711a3ff21d7bf8855d966ca3150aa67e
ouailb <adomeyou@gmail.com>
2024-01-07 14:48:41 +0100

    t+

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,13 +4,12 @@
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
-import urllib.request
 import sys
+import urllib.request
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-req = urllib.request.Request(url)
-
-with urllib.request.urlopen(req) as response:
-    x_request_id = response.getheader('X-Request-Id')
-    print(x_request_id)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
